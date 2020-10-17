bf9f5b67e0f97c5b7d7bab5301b71eda848b549f
Rodrigo Barbosa <rodrigo.lima@agriconnected.com>
2020-10-17 14:50:06 -0300

    styled landing page

--- src/pages/Landing/index.tsx
+++ src/pages/Landing/index.tsx
@@ -1,11 +1,95 @@
-import React from 'react';
+import React from "react";
 
-import { Container } from './styles';
+import {
+  Container,
+  Header,
+  Image,
+  HeaderUL,
+  HeaderLI,
+  Content,
+  TextBox,
+  ImageBox,
+  Thumb,
+  SectionIcons
+} from "./styles";
+
+import logoImage from "../../assets/images/logo.png";
+import pepsiImage from "../../assets/images/pepsi001.png";
+
+import iconFB from "../../assets/images/facebook.png";
+import iconTw from "../../assets/images/twitter.png";
+import iconInst from "../../assets/images/instagram.png";
+
+import pepsiImage2 from "../../assets/images/pepsi002.png";
+import pepsiImage3 from "../../assets/images/pepsi003.png";
 
 const Landing: React.FC = () => {
   return (
     <Container>
-      <h1>Landing</h1>
+      <Header>
+        <a href="#" rel="noopener noreferrer">
+          <Image>
+            {" "}
+            <img src={logoImage} alt="" />
+          </Image>
+        </a>
+        <HeaderUL>
+          {/* <li> */}
+          <HeaderLI>
+            <a href="">Home</a>
+          </HeaderLI>
+          <HeaderLI>
+            <a href="">Products</a>
+          </HeaderLI>
+          <HeaderLI>
+            <a href="">What's New</a>
+          </HeaderLI>
+          <HeaderLI>
+            <a href="">Newsletter</a>
+          </HeaderLI>
+          <HeaderLI>
+            <a href="">Contact</a>
+          </HeaderLI>
+
+          {/* </li> */}
+        </HeaderUL>
+      </Header>
+      <Content>
+        <TextBox>
+          <h2>
+            That's What'
+            <br />
+            <span>I Like</span>
+          </h2>
+          <p>Lorem ipsum</p>
+          <a href="#">View All Products</a>
+        </TextBox>
+        <ImageBox>
+          <img src={pepsiImage} alt="" className="pepsi" />
+        </ImageBox>
+      </Content>
+      <Thumb>
+        <li>
+          <img src={pepsiImage} alt="blue" />
+        </li>
+        <li>
+          <img src={pepsiImage2} alt="gray" />
+        </li>
+        <li>
+          <img src={pepsiImage3} alt="black" />
+        </li>
+      </Thumb>
+      <SectionIcons>
+        <li>
+          <img src={iconInst} alt="" />
+        </li>
+        <li>
+          <img src={iconTw} alt="" />
+        </li>
+        <li>
+          <img src={iconFB} alt="" />
+        </li>
+      </SectionIcons>
     </Container>
   );
 };
--- src/pages/Landing/styles.ts
+++ src/pages/Landing/styles.ts
@@ -1,5 +1,143 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
+  position: relative;
+  width: 100%;
+  min-height: 100vh;
+  padding: 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: #0062be;
+  transition: 0.5s;
+`;
+export const Header = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  padding: 20px 100px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
+export const Image = styled.image`
+  position: relative;
+  max-width: 80px;
+`;
+
+export const HeaderUL = styled.ul`
+  position: relative;
+  display: flex;
+`;
+
+export const HeaderLI = styled.li`
+  list-style: none;
+
+  > a {
+    display: inline-block;
+    color: #fff;
+    font-weight: 100;
+    margin-left: 40px;
+    text-decoration: none;
+  }
+`;
+
+export const Content = styled.div`
+  position: relative;
+  width: 100%;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TextBox = styled.div`
+  position: relative;
+  max-width: 600px;
+
+  > h2 {
+    color: #fff;
+    font-size: 4em;
+    line-height: 1.5em;
+    font-weight: 900;
+    text-transform: uppercase;
+  }
+
+  > span {
+    font-size: 2em;
+  }
+  > p {
+    color: #fff;
+  }
+  > a {
+    display: inline-block;
+    margin-top: 20px;
+    padding: 8px 20px;
+    background-color: #fff;
+    color: #111;
+    border-radius: 40px;
+    font-weight: 500;
+    letter-spacing: 1px;
+    text-decoration: none;
+    text-transform: uppercase;
+  }
+`;
+
+export const ImageBox = styled.image`
+  width: 600px;
+  display: flex;
+  padding: 50px;
+  margin-top: 50px;
+  justify-content: flex-end;
+
+  > img {
+    max-width: 260px;
+  }
+`;
+export const Thumb = styled.ul`
+  position: absolute;
+  left: 50%;
+  bottom: 20px;
+  transform: translateX(-50%);
+  display: flex;
+  > li {
+    list-style: none;
+    display: inline-block;
+    margin: 0 20px;
+    cursor: pointer;
+    transition: 0.5s;
+
+    img {
+      max-width: 40px;
+    }
+  }
+  > li:hover {
+    transform: translateY(-15%);
+  }
+`;
+
+export const SectionIcons = styled.ul`
+  position: absolute;
+  transform: translateY(-50%);
+  top: 50%;
+  right: 30px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  flex-direction: column;
+
+  > li {
+    list-style: none;
+
+    > a {
+      display: inline-block;
+      filter: invert(1);
+      margin: 5px 0;
+      transform: scale(0.6);
+    }
+  }
 `;
